ac419e5adee93183626778b9d1d5c338b4af4dc2
Disty0 <semihgulec2005@gmail.com>
2020-04-19 23:57:09 +0300

    modified:   detectnumber.py 	modified:   main.py

--- detectnumber.py
+++ detectnumber.py
@@ -9,8 +9,6 @@ def DetectNumber(Screen, Speed_Region_Y1, Speed_Region_Y2, Speed_Region_X1, Spee
 
     SpeedImg = cv2.resize(SpeedImg, ((Speed_Region_X2-Speed_Region_X1)*Scale, (Speed_Region_Y2-Speed_Region_Y1)*Scale))
 
-    #SpeedImg = cv2.medianBlur(SpeedImg, 1)
-
     Speed = pytesseract.image_to_string(SpeedImg, config='-c tessedit_char_whitelist=0123456789')
 
     return (Speed, SpeedImg)
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2
 import time
-from grabscreen import grab_screen
-import detect
 import pyautogui
 import actions
 import pytesseract
-import detectnumber
+from grabscreen import grab_screen
+from detect import detect
+from detectnumber import DetectNumber
 
 """for i in list(range(4))[::-1]:
     print(i+1)
@@ -17,19 +17,21 @@ last_time = time.time()
 while True:
 
     Screen = grab_screen((1280,65,1024,768))
-    #(output_img, bbox, label, conf) = detect.detect(screen) #object detection
-
-    (Speed, SpeedImg) = detectnumber.DetectNumber(Screen,  480,490, 776,795, Scale=1)
+    
+    (Speed, SpeedImg) = DetectNumber(Screen,  480,490, 776,795, Scale=1)
 
     print("Speed: {}".format(Speed))
 
-    #print('Frame took {} seconds'.format(time.time()-last_time))
-    #last_time = time.time()
-
-    #cv2.imshow('Screen', cv2.resize(Screen, (768,576)))
-
     cv2.imshow('SpeedImg', SpeedImg)
 
+    #(output_img, bbox, label, conf) = detect(Screen) #object detection
+    
+    #cv2.imshow('Screen', cv2.resize(output_img, (768,576)))
+
+    """print('Frame took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()"""
+
+    
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
